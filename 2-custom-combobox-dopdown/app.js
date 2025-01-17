f57b17ec623b8c152cdf52efbd48b6b1e4010b0a
Avery Maxwell <averymaxwell42@gmail.com>
2025-01-16 21:47:53 -0600

    Create movieLibrary array, populate with movies

---
+++ 2-custom-combobox-dopdown/app.js
@@ -0,0 +1,34 @@
+const comboboxContainer = document.querySelector('.combobox-container');
+
+let movieLibrary = [
+    {
+        title: 'Christmas Carol',
+        year: 1951,
+        img: 'christmas-carol.png',
+    },
+    {
+        title: `National Lampoon's Christmas Vacation`,
+        year: 1989,
+        img: `christmas-vacation.png`,
+    },
+    {
+        title: `Die Hard`,
+        year: 1988,
+        img: `die-hard.png`,
+    },
+    {
+        title: `Home Alone`,
+        year: 1990,
+        img: `home-alone.png`,
+    },
+    {
+        title: `It's a Wonderful Life`,
+        year: 1946,
+        img: `its-a-wonderful-life.png`,
+    },
+    {
+        title: `Rudolph the Red-Nosed Reindeer`,
+        year: 1964,
+        img: `rudolph-the-red-nosed-reindeer.png`,
+    },
+]
